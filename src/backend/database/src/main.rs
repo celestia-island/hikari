3763b92291ba50155a70d7457a84faf69c1d4fb1
伊欧 <langyo.china@gmail.com>
2023-01-13 03:13:29 +0800

    🚀

--- src/backend/database/src/main.rs
+++ src/backend/database/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+mod migration;
+mod models;
+
+use sea_orm::{Database, EntityTrait};
+use sea_orm_migration::MigratorTrait;
+
+#[tokio::main]
+async fn main() {
+    let db = Database::connect("mysql://root:1145141919810@localhost:3306/test")
+        .await
+        .unwrap();
+    println!("{:?}\n", db);
+
+    migration::Migrator::up(&db, None).await.unwrap();
+
+    for cc in models::post::Entity::find().all(&db).await.unwrap() {
+        println!("{:?}\n", cc);
+    }
 }

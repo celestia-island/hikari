d2b99eec11cb3777ac2ef4b73892c9c869da2cef
伊欧 <langyo.china@gmail.com>
2023-01-19 10:45:24 +0800

    ⚗️ Try to use functional component hooks.

--- src/frontend/web/src/pages/home.rs
+++ src/frontend/web/src/pages/home.rs
@@ -1,23 +1,53 @@
+use std::rc::Rc;
+
+use gloo::console::console;
+use gloo::net::http::Request;
 use stylist::yew::styled_component;
 use yew::prelude::*;
 
 #[styled_component]
 pub fn Inside() -> Html {
+    let count = use_state(|| Rc::new(".".to_string()));
+
+    use_effect_with_deps(
+        move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let response = Request::get("https://httpbin.org/get")
+                    .send()
+                    .await
+                    .unwrap();
+                let raw = response.text().await.unwrap();
+                console!(raw);
+            })
+        },
+        (),
+    );
+
+    let onclick = {
+        let count = count.clone();
+        Callback::from(move |_| {
+            count.set((*count).repeat(2).into());
+        })
+    };
+
     html! {
-        <div class={css!(r#"
-            width: 200px;
-            height: 200px;
-            border-radius: 5px;
+        <div
+            class={css!(r#"
+                width: 200px;
+                height: 200px;
+                border-radius: 5px;
 
-            background: black;
+                background: black;
 
-            padding: 15px;
-            box-sizing: border-box;
+                padding: 15px;
+                box-sizing: border-box;
 
-            box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.7);
-            color: white;
-        "#)}>
-            {"The quick brown fox jumps over the lazy dog"}
+                box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.7);
+                color: white;
+            "#)}
+            onclick={onclick}
+        >
+            {&**count}
         </div>
     }
 }

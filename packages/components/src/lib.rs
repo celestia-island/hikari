23ed59b1cd12a24d23d14f626bdec2b51da56fea
伊欧 <langyo.china@gmail.com>
2023-11-07 11:05:24 +0800

    🏗️ Move the components to the root folder.

--- packages/components/src/lib.rs
+++ packages/components/src/lib.rs
@@ -1,3 +1,21 @@
-mod components;
+pub mod container;
+pub mod data;
+pub mod form;
+pub mod navigation;
 
-pub use components::*;
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Size {
+    Small,
+    Medium,
+    Large,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Color {
+    Primary,
+    Secondary,
+    Success,
+    Error,
+    Info,
+    Warning,
+}

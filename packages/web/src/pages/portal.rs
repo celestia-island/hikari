a77448c46470254172d655f4ff2c5730754d1761
伊欧 <langyo.china@gmail.com>
2023-11-24 13:54:10 +0800

    ✨ Now it can dragging by transparent window.

--- packages/web/src/pages/portal.rs
+++ packages/web/src/pages/portal.rs
@@ -47,6 +47,23 @@ pub fn Portal() -> Html {
 
     html! {
         <>
+            <div
+                class={
+                    css!(
+                        r#"
+                            width: 100%;
+                            height: 48px;
+                            background: blue;
+                            position: fixed;
+                            top: 0;
+                            left: 0;
+                            user-select: none;
+                            z-index: 10000;
+                        "#
+                    )
+                }
+                data-tauri-drag-region={""}
+            />
             <TextInput
                 value={
                     (*(uri.clone())).clone()

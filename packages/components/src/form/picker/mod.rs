3ff2a2866054b424032c4a549695988be0bfbf5e
伊欧 <langyo.china@gmail.com>
2024-10-27 20:09:58 +0800

    🚧 Add empty module.

--- packages/components/src/form/picker/mod.rs
+++ packages/components/src/form/picker/mod.rs
@@ -1,9 +1,11 @@
 mod color;
 mod date;
 mod date_time;
+mod file;
 mod time;
 
 pub use color::ColorPicker;
 pub use date::DatePicker;
 pub use date_time::DateTimePicker;
+pub use file::FilePicker;
 pub use time::TimePicker;

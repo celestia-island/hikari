9c37a18746ff84d37439a37d39e15f9d88de1c48
langyo <langyo.china@gmail.com>
2026-02-14 04:33:48 +0800

    新增选择详情弹窗组件，支持自定义选项渲染和样式

--- examples/website/src/pages/components/layer1/mod.rs
+++ examples/website/src/pages/components/layer1/mod.rs
@@ -6,7 +6,6 @@ pub mod basic_components;
 pub mod display;
 pub mod feedback;
 pub mod form;
-pub mod select_detail_modal;
 pub mod switch;
 
 // Re-exports
@@ -15,5 +14,4 @@ pub use basic_components::*;
 pub use display::Layer1Display;
 pub use feedback::Layer1Feedback;
 pub use form::Layer1Form;
-pub use select_detail_modal::SelectDetailModal;
 pub use switch::Layer1Switch;

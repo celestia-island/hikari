c41624e5a9c88a83d6278680b9c3ef41316ab625
lab <lab@celestia.world>
2026-08-05 14:48:22 +0800

    🐛 Restore the dialog-style useConfirm shadow copy with live downstream consumers.

--- packages/vue/src/runtime/index.ts
+++ packages/vue/src/runtime/index.ts
@@ -3,6 +3,7 @@ export * from "./cronBus";
 export { useOverlay, closeAll, isOverlayOpen, type OverlayHandle, type UseOverlayOptions } from "./useOverlay";
 export { usePopupManager, type PopupHandle, type PopupKind } from "./usePopupManager";
 export { useToast, TOAST_DURATION, type ToastItem, type ToastMessage, type ToastType } from "./useToast";
+export { useConfirm } from "./useConfirm";
 export { useBreakpoint } from "./useBreakpoint";
 export { useClipboard } from "./useClipboard";
 export { provideScrollWindow, useScrollWindow, type ScrollWindowContext } from "../composables/useScrollWindow";
